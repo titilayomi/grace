294fe56cc09d39b66b6249924f1679f37a187c4a
titilayo <oluwabamiseo@student.swosu.edu>
2016-11-01 15:16:45 -0500

    added freshies to grace

---
+++ team project 1/src/freshies.java
@@ -0,0 +1,11 @@
+
+public class freshies {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("yeppie are we good to go");
+System.out.println("i think so");
+
+	}
+
+}
